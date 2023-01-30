b4e283e7a4998fe133991b1ce3dbbcde1ba5b597
Waldo Sobrino <waldo2810@gmail.com>
2023-01-30 14:58:09 -0500

    finish backend api's. add auth middlewares, also posts/users controllers and routes. add post model.

---
+++ server/routes/posts.js
@@ -0,0 +1,14 @@
+import express from "express";
+import { getFeedPosts, getUserPosts, likePost } from "../controllers/posts";
+import { verifyToken } from "../middleware/auth";
+
+const router = express.Router();
+
+//! READ
+router.get("/", verifyToken, getFeedPosts);
+router.get("/:userId/posts", verifyToken, getUserPosts);
+
+//! UPDATE
+router.patch("/:id/like", verifyToken, likePost);
+
+export default router;
