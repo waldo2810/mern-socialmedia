8dbf9f9f48eb1ebe7f3ec66bec0602203f8ea969
Waldo Sobrino <waldo2810@gmail.com>
2023-02-24 16:50:51 -0500

    multer now working

--- server/index.js
+++ server/index.js
@@ -35,7 +35,7 @@ app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 /* FILE STORAGE */
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "public/assets");
+    cb(null, "");
   },
   filename: function (req, file, cb) {
     cb(null, file.originalname);
